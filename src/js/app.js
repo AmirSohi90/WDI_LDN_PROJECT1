e27b6deba83b07ab4bec502bb24e4fff4e6382a3
Amir Sohi <amirasohi@gmail.com>
2018-02-09 16:38:46 +0000

    added click event on timer

--- src/js/app.js
+++ src/js/app.js
@@ -11,19 +11,43 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const start = document.querySelector('.start');
   console.log(start);
-
+  //random time for food to pop up and down
   function randomTime(min, max) {
     const popUpTime = Math.round(Math.random() * (max - min) + min);
     return popUpTime;
   }
+
+  let timer = 30;
+  countdown.innerHTML = timer;
+
   console.log(randomTime(200, 2000));
 
+  //function to get random pots
   function randomPot(pots){
     const potIndex = Math.floor(Math.random() * pots.length);
-    // console.log(potIndex);
-    // console.log(pots[0]);
     const pot = pots[potIndex];
     return pot;
   }
   console.log(randomPot(pots));
+
+  let timerId = null;
+  let runningTimer = false;
+
+  start.addEventListener('click', function(){
+    if(!runningTimer){
+      runningTimer = true;
+      timerId = setInterval(() => {
+        timer -=  1;
+        if(timer === 0){
+          clearInterval(timerId);
+        }
+        countdown.innerHTML = timer;
+        console.log(timer);
+      }, 1000);
+    } else {
+      runningTimer = false;
+      clearInterval(timerId);
+    }
+  });
+
 });
