a059950de5f2db427b74b43d00aac57ab229a4e1
Amir Sohi <amirasohi@gmail.com>
2018-02-09 15:57:29 +0000

    made function for random time and moving onto making a random pot function

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  const countdown = document.querySelector('.timer-countdown');
+  console.log(countdown);
 
+  const scoreCount = document.querySelector('.score');
+  console.log(scoreCount);
 
+  const pots = document.querySelectorAll('.whack');
+  console.log(pots);
+
+  const start = document.querySelector('.start');
+  console.log(start);
+
+  function randomTime(min, max) {
+    let popUpTime = Math.round(Math.random() * (max - min) + min);
+    return popUpTime;
+  }
+  console.log(randomTime(200, 2000));
 });
