b246aa5a9426e4abe364aae8131a507d4ea3b147
Amir Sohi <amirasohi@gmail.com>
2018-02-10 21:21:37 +0000

    made popup color be random

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,13 @@
 let timer = 10;
-// let gameOn = false;
 let lastPot;
 let timeUp = false;
 let scoreCounter = 0;
+
 window.addEventListener('DOMContentLoaded', () => {
 
   const countdown = document.querySelector('.timer-countdown');
 
   const score = document.querySelector('.score-board');
-
-  const start = document.querySelector('.start-div');
 
   const startBtn = document.querySelector('.start-button');
 
@@ -35,12 +33,21 @@ window.addEventListener('DOMContentLoaded', () => {
     return pot;
   }
 
+
   function peak (){
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    const newColor = 'rgb(' + x + ',' + y + ',' + z + ')';
     const time = randomTime(100, 1000);
     const pot = randomPot(pots);
     pot.classList.add('change-color');
+    if(pot.classList.contains('change-color')){
+      pot.style.background = newColor;
+    }
     setTimeout(() => {
       pot.classList.remove('change-color');
+      pot.style.background = 'pink';
       if(!timeUp) peak();
     }, time);
     return time, pot;
@@ -48,10 +55,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function startGame() {
     startBtn.disabled = true;
-    // gameOn = true;
-    // if(gameOn === true){
-    //   start.classList.add('hide');
-    // }
     timer = 10;
     countdown.textContent = timer;
     scoreCounter = 0;
@@ -63,10 +66,6 @@ window.addEventListener('DOMContentLoaded', () => {
       countdown.textContent = timer;
       if(timer === 0){
         startBtn.disabled = false;
-        // gameOn = false;
-        // if(gameOn === false){
-        //   start.classList.remove('hide');
-        // }
         clearInterval(timerId);
         timeUp = true;
       }
